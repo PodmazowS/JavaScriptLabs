c6cd58d49a46ad5e4915e40614039c267bd3ca67
PodmazowS <podmazovstas@gmail.com>
2023-10-28 17:09:20 +0200

    Lab2

---
+++ Lab2/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = Array.from(document.querySelectorAll(".slide"));
+    const [btnLeft, btnRight] = Array.from(document.querySelectorAll(".slider__btn"));
+    const dotContainer = document.querySelector(".dots");
+  
+    let curSlide = 0;
+  
+    const goToSlide = slide => {
+      slides.forEach((s, i) => s.style.transform = `translateX(${(i - slide) * 100}%)`);
+    };
+  
+    const nextSlide = () => {
+      curSlide = curSlide === slides.length - 1 ? 0 : curSlide + 1;
+      goToSlide(curSlide);
+    };
+  
+    const prevSlide = () => {
+      curSlide = curSlide === 0 ? slides.length - 1 : curSlide - 1;
+      goToSlide(curSlide);
+    };
+  
+    btnRight.addEventListener("click", nextSlide);
+    btnLeft.addEventListener("click", prevSlide);
+  
+    goToSlide(0);
+  });
